ff6cc7c78b3d53119b1483d96b6dd7093fe8cf63
git-luiz <luiz.brey@plss.com.br>
2021-07-07 16:45:41 -0300

    cripto

--- criptography/criptography.py
+++ criptography/criptography.py
@@ -1,4 +1,6 @@
 import sys
+import string
+import random
 
 
 class Cryptography:
@@ -6,70 +8,106 @@ class Cryptography:
     def encrypt(self,arquivo):
         ref_arquivo = open(arquivo, "r")
         arquivo2 = open("arquivoSaida.txt", "w+")
+
+        tam = 26
+        print (tam)
+        key = random.sample(string.ascii_uppercase, tam)
+        key2=[]
+        key1=[]
+        for i in range(13,25):
+            key2.append(key[i])
+        for i in range(0,12):
+            key1.append(key[i])
+
+        print(key1)
+        print(key2)
+
+
+        print(key)
         for line in ref_arquivo:
 
             for i in list(line):
-                i=i.lower()
-                if i == "l":
-                    arquivo2.write("J")
-                elif i == "b":
-                    arquivo2.write("O")
-                elif i == "r":
-                    arquivo2.write("H")
-                elif i == "e":
-                    arquivo2.write("A")
-                elif i == "y":
-                    arquivo2.write("N")
-                elif i == "j":
-                    arquivo2.write("L")
-                elif i == "o":
-                    arquivo2.write("B")
-                elif i == "h":
-                    arquivo2.write("R")
-                elif i == "a":
-                    arquivo2.write("E")
-                elif i == "n":
-                    arquivo2.write("Y")
+                i = i.upper()
+
+                if i == key1:
+                    arquivo2.write(key2)
+                elif i == key2:
+                    arquivo2.write(key1)
                 else:
                     arquivo2.write(i.upper())
 
-    def decrypt(self,arquivo):
+
+
+
+                # if i == key[0]:
+                #     arquivo2.write(key[1])
+                # elif i == key[2]:
+                #     arquivo2.write(key[3])
+                # elif i == key[4]:
+                #     arquivo2.write(key[5])
+                # elif i == key[6]:
+                #     arquivo2.write(key[7])
+                # elif i == key[8]:
+                #     arquivo2.write(key[9])
+                # elif i == key[10]:
+                #     arquivo2.write(key[11])
+                # elif i == key[1]:
+                #     arquivo2.write(key[0])
+                # elif i == key[3]:
+                #     arquivo2.write(key[2])
+                # elif i == key[5]:
+                #     arquivo2.write(key[4])
+                # elif i == key[7]:
+                #     arquivo2.write(key[6])
+                # elif i == key[9]:
+                #     arquivo2.write(key[8])
+                # elif i == key[11]:
+                #     arquivo2.write(key[10])
+                # else:
+                #     arquivo2.write(i.upper())
+
+    def decrypt(self,arquivo,key):
         ref_arquivo = open(arquivo, "r")
         arquivo2 = open("arquivoOriginal.txt", "w+")
         for line in ref_arquivo:
 
             for i in list(line):
-                i=i.lower()
-                if i == "l":
-                    arquivo2.write("J")
-                elif i == "b":
-                    arquivo2.write("O")
-                elif i == "r":
-                    arquivo2.write("H")
-                elif i == "e":
-                    arquivo2.write("A")
-                elif i == "y":
-                    arquivo2.write("N")
-                elif i == "j":
-                    arquivo2.write("L")
-                elif i == "o":
-                    arquivo2.write("B")
-                elif i == "h":
-                    arquivo2.write("R")
-                elif i == "a":
-                    arquivo2.write("E")
-                elif i == "n":
-                    arquivo2.write("Y")
+                i = i.upper()
+                if i == key[0]:
+                    arquivo2.write(key[1])
+                elif i == key[2]:
+                    arquivo2.write(key[3])
+                elif i == key[4]:
+                    arquivo2.write(key[5])
+                elif i == key[6]:
+                    arquivo2.write(key[7])
+                elif i == key[8]:
+                    arquivo2.write(key[9])
+                elif i == key[10]:
+                    arquivo2.write(key[11])
+                elif i == key[1]:
+                    arquivo2.write(key[0])
+                elif i == key[3]:
+                    arquivo2.write(key[2])
+                elif i == key[5]:
+                    arquivo2.write(key[4])
+                elif i == key[7]:
+                    arquivo2.write(key[6])
+                elif i == key[9]:
+                    arquivo2.write(key[8])
+                elif i == key[11]:
+                    arquivo2.write(key[10])
                 else:
                     arquivo2.write(i.upper())
 
 
 class Main:
+
     string_arquivo = "arquivoEntrada.txt"
     cripto= Cryptography()
     cripto.encrypt(string_arquivo)
     string_arquivo2="arquivoSaida.txt"
-    cripto.decrypt(string_arquivo2)
+    #cripto.decrypt(string_arquivo2,"FRCMDGXSPOYI")
 
 
 
