527e99f9ce17c4aafd01cd8d3c557fa1ea12b3e0
vector_alecsander-at-693885258016 <NkXeom9tzbu63shGMKGkA0xJV/TLBbFgOHQokE6umQk=>
2021-07-22 20:45:42 -0300

    Ajustes para desktop e web

--- criptography/criptography.py
+++ criptography/criptography.py
@@ -1,111 +1,42 @@
-import sys
 import string
 import random
 import os
-from flask import Flask, request, make_response, Response, send_file, jsonify, json
-from werkzeug.utils import secure_filename
-app = Flask (__name__)
-app.config['UPLOAD_FOLDER'] = "~/Área de trabalho/cripto-trabalho"
-app.config['MAX_CONTENT_PATH'] = "9090000"
-# class Cryptography:
 
-@app.route('/encrypt/', methods=['POST'])
-def encrypt():
-    arquivo = request.files['file']
-    arquivo.save(arquivo.filename)
-    ref_arquivo = open(arquivo.filename, "r")
-    arquivo2 = open("arquivoSaida.txt", "w+")
+class Cryptography:
+    def encrypt(self,text): 
+        return self.alg(text, None)
 
-    tam = 26
-    key = random.sample(string.ascii_uppercase, tam)
-    key2=[]
-    key1=[]
-    for i in range(13,26):
-        key2.append(key[i])
-    for i in range(0,13):
-        key1.append(key[i])
+    def decrypt(self,text,key):
+        return self.alg(text, key)[1]
 
-    for line in ref_arquivo:
+    def alg(self, text, key):
+        tam = 26
+        result = ""
+        key2=[]
+        key1=[]
+        if key is None:
+            key = random.sample(string.ascii_uppercase, tam)
 
-        for i in list(line):
+        for i in range(13, 26):
+            key2.append(key[i])
+        for i in range(0, 13):
+            key1.append(key[i])
+
+        for i in list(text):
             i = i.upper()
             j=0
             contador = 0
             x = 0
             while contador != 1:
                 if i == key2[j]:
-                    arquivo2.write(key1[j])
+                    result = result + key1[j]
                     contador = 1
                 elif i == key1[j]:
-                    arquivo2.write(key2[j])
+                    result = result + key2[j]
                     contador = 1
                 elif j == 12:
-                    arquivo2.write(i.upper())
+                    result = result + i.upper()
                     contador=1
                 j = j+1
-    print("\n" * 130)
-    chave = "".join(key)
-    for letter in key:
-        print(letter, end ='')
-
-    print("\n")
-    #return send_file(path_or_file="arquivoSaida.txt")
-    return send_file(path_or_file="arquivoSaida.txt"), chave
-
-
-@app.route('/decrypt/', methods=['POST'])
-def decrypt(self,request):
-    key = request.data['key']
-    arquivo = request.files['file']
-    ref_arquivo = open(arquivo, "r")
-    arquivo2 = open("arquivoOriginal.txt", "w+")
-
-    tam = 26
-    key2 = []
-    key1 = []
-    for i in range(13, 26):
-        key2.append(key[i])
-    for i in range(0, 13):
-        key1.append(key[i])
-
-    for line in ref_arquivo:
-
-        for i in list(line):
-            i = i.upper()
-            j = 0
-            contador = 0
-            x = 0
-            while contador != 1:
-                if i == key2[j]:
-                    arquivo2.write(key1[j])
-                    contador = 1
-                elif i == key1[j]:
-                    arquivo2.write(key2[j])
-                    contador = 1
-                elif j == 12:
-                    arquivo2.write(i.upper())
-                    contador = 1
-                j = j + 1
-    print("\n" * 130)
-
-    return arquivo2
-
-app.run(debug = True)
-
-# class Main:
-
-    # opcao = 1
-    # while opcao != 0:
-    #
-    #     opcao = input ('0- Sair \n 1- Criptografar \n 2-Decriptografar \n')
-    #     string_arquivo = "arquivoEntrada.txt"
-    #     cripto = Cryptography()
-    #     string_arquivo2 = "arquivoSaida.txt"
-    #
-    #     if opcao == '1':
-    #         print(cripto.encrypt(string_arquivo))
-    #     elif opcao == '2':
-    #         key = input ('Informe a chave\n')
-    #         cripto.decrypt(string_arquivo2, key)
-    #     elif opcao == '0':
-    #         sys.exit()
+        retorno = (key, result)
+        return retorno
---
+++ criptography/criptographyDesktop.py
@@ -0,0 +1,63 @@
+import string
+import random
+import os
+import criptography
+
+
+class SendCryptography:
+    
+    def sendEncrypt(self,arquivo):
+        print("Lendo arquivo...")
+        ref_arquivo = open(arquivo, "r")
+        
+        cripto = criptography.Cryptography()
+        text = ""
+
+        for line in ref_arquivo:
+            text = text + line
+
+        print("Encriptando conteudo do arquivo...")
+        retorno = cripto.encrypt(text)
+        file2 = open(retorno[0], "w+")  
+        file2.write(retorno[1])
+        print("Conteúdo Criptografado, Chave:")
+        for letter in key:
+            print(letter, end ='')
+
+        print ("\n")
+
+    def sendDecrypt(self,arquivo,key):
+        print("Lendo arquivo...")
+        ref_arquivo = open(arquivo, "r")
+        arquivo2 = open("arquivoOriginal.txt", "w+")
+        cripto = SendCryptography()
+        if len(list(key)) == 26:
+            print("Chave de tamanho inválido.")
+            return
+
+        text = ""
+        for line in ref_arquivo:
+            text = text + line
+
+        print("Decriptando conteudo do arquivo...")
+        retorno = cripto.decrypt(text, key)
+        arquivo2.write(retorno)
+        return
+    
+class Main:
+
+    opcao = 1
+    while opcao != 0:
+
+        opcao = input ('0- Sair \n 1- Criptografar \n 2-Decriptografar \n')
+        string_arquivo = "arquivoEntrada.txt"
+        cripto = SendCryptography()
+        string_arquivo2 = "arquivoSaida.txt"
+
+        if opcao == '1':
+            print(cripto.sendEncrypt(string_arquivo))
+        elif opcao == '2':
+            key = input ('Informe a chave\n')
+            cripto.sendDecrypt(string_arquivo2, key)
+        elif opcao == '0':
+            sys.exit()
